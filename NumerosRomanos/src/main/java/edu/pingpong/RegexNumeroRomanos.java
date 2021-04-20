50347714706fa6ca58285400c3e9b08c513d0c2a
AliciaTugores <atugoresb@cifpfbmoll.eu>
2021-04-20 19:49:25 +0200

    feat(prácticamente es un copia/pega del código de dfleta, por que no conseguía entenderlo y de esta manera he ido conectando las cosas)

---
+++ NumerosRomanos/src/main/java/edu/pingpong/RegexNumeroRomanos.java
@@ -0,0 +1,30 @@
+package edu.pingpong;
+
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+
+public class RegexNumeroRomanos {
+
+    private Map<String, String> regexDiccionario = new HashMap<String, String>();
+
+    public void addRegex(String key, String value){
+        this.regexDiccionario.put(key, value);
+    }
+
+    public Map<String, String> getRegex() {
+        return this.regexDiccionario;
+    }
+
+    public String getRegexValue(String key) {
+        return this.regexDiccionario.get(key);
+    }
+
+    public Collection<String> getValues() {
+        return this.regexDiccionario.values();
+    }
+
+    public long longitud() {
+        return this.regexDiccionario.size();
+    }
+}
